cb10afcfc691f9227aaf759a16f0af0cc9082326
Ilya Puchka <ilya@puchka.me>
2015-04-06 00:37:52 +0300

    example project added

---
+++ Example/ReadMoreTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadMoreTextView
+//
+//  Created by Ilya Puchka on 06.04.15.
+//  Copyright (c) 2015 Ilya Puchka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

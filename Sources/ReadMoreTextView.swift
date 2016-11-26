ed9d0d66a3af46617635306d54a67ca215526c99
Ilya Puchka <ilyapuchka@gmail.com>
2016-11-26 21:59:23 +0100

    updated licence

--- Sources/ReadMoreTextView.swift
+++ Sources/ReadMoreTextView.swift
@@ -3,7 +3,7 @@
 //  ReadMoreTextView
 //
 //  Created by Ilya Puchka on 06.04.15.
-//  Copyright (c) 2015 Ilya Puchka. All rights reserved.
+//  Copyright (c) 2015 - 2016 Ilya Puchka. All rights reserved.
 //
 
 import UIKit

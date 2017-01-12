c8007983bf578fc9d603fb2f2589c66327fe121e
Ilya Puchka <ilyapuchka@gmail.com>
2017-01-12 20:50:00 +0100

    reintegrated with pods

--- Example/Pods/Target Support Files/Pods-ReadMoreTextViewExample/Pods-ReadMoreTextViewExample-umbrella.h
+++ Example/Pods/Target Support Files/Pods-ReadMoreTextViewExample/Pods-ReadMoreTextViewExample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ReadMoreTextViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ReadMoreTextViewVersionString[];
+FOUNDATION_EXPORT double Pods_ReadMoreTextViewExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ReadMoreTextViewExampleVersionString[];
 
--- Example/ReadMoreTextView/ViewController.swift
+++ Example/ReadMoreTextView/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         readMoreTextView.text = "Lorem http://ipsum.com dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
         #if swift(>=3.0)
-            let readMoreTextAttributes = [
+            let readMoreTextAttributes: [String: Any] = [
                 NSForegroundColorAttributeName: view.tintColor,
                 NSFontAttributeName: UIFont.boldSystemFont(ofSize: 16)
             ]

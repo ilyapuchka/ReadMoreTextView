8775e7519eb9d1158664e1189ccaee012981364c
Adam Eri <adam.eri@blackmirror.media>
2017-09-26 13:39:27 +0200

    Converted to Swift 4.

--- Example/ReadMoreTextView/ViewController.swift
+++ Example/ReadMoreTextView/ViewController.swift
@@ -24,13 +24,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         
         readMoreTextView.text = "Lorem http://ipsum.com dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
-        let readMoreTextAttributes: [String: Any] = [
-            NSForegroundColorAttributeName: view.tintColor,
-            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 16)
+        let readMoreTextAttributes: [NSAttributedStringKey: Any] = [
+          NSAttributedStringKey.foregroundColor: view.tintColor,
+          NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 16)
         ]
         let readLessTextAttributes = [
-            NSForegroundColorAttributeName: UIColor.red,
-            NSFontAttributeName: UIFont.italicSystemFont(ofSize: 16)
+          NSAttributedStringKey.foregroundColor: UIColor.red,
+          NSAttributedStringKey.font: UIFont.italicSystemFont(ofSize: 16)
         ]
         readMoreTextView.attributedReadMoreText = NSAttributedString(string: "... Read more", attributes: readMoreTextAttributes)
         readMoreTextView.attributedReadLessText = NSAttributedString(string: " Read less", attributes: readLessTextAttributes)

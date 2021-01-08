e73db0d89779bf513c5319e41af6dac98a035b53
Igor Palaguta <igor.palaguta@gmail.com>
2021-01-08 10:30:55 +0100

    Don't override hitTest

--- Example/ReadMoreTextView/ViewController.swift
+++ Example/ReadMoreTextView/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView! {
         didSet {
             tableView.estimatedRowHeight = 100
-            tableView.rowHeight = UITableViewAutomaticDimension
+            tableView.rowHeight = UITableView.automaticDimension
         }
     }
 
@@ -24,13 +24,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         
         readMoreTextView.text = "👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦👨‍👩‍👧‍👦 Lorem http://ipsum.com dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
-        let readMoreTextAttributes: [NSAttributedStringKey: Any] = [
-          NSAttributedStringKey.foregroundColor: view.tintColor,
-          NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 16)
+        let readMoreTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: view.tintColor,
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16)
         ]
         let readLessTextAttributes = [
-          NSAttributedStringKey.foregroundColor: UIColor.red,
-          NSAttributedStringKey.font: UIFont.italicSystemFont(ofSize: 16)
+            NSAttributedString.Key.foregroundColor: UIColor.red,
+            NSAttributedString.Key.font: UIFont.italicSystemFont(ofSize: 16)
         ]
         readMoreTextView.attributedReadMoreText = NSAttributedString(string: "... Read more", attributes: readMoreTextAttributes)
         readMoreTextView.attributedReadLessText = NSAttributedString(string: " Read less", attributes: readLessTextAttributes)
